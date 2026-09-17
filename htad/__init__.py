d5577439b312c581d78f04a250a31c5f77110228
s-dp-j <104260646+s-dp-j@users.noreply.github.com>
2026-09-17 18:56:27 +0800

    Delete htad directory

--- htad/__init__.py
+++
@@ -1,6 +0,0 @@
-"""CrossT-GAN: a reproducible implementation for multivariate anomaly detection."""
-
-from .model import CrossTGAN, MLPDiscriminator
-
-__all__ = ["CrossTGAN", "MLPDiscriminator"]
-
