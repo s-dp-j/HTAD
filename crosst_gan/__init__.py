55640219b02c849d4ce192d113c727657d6e4a99
s-dp-j <104260646+s-dp-j@users.noreply.github.com>
2026-09-17 18:48:03 +0800

    Add files via upload

---
+++ crosst_gan/__init__.py
@@ -0,0 +1,6 @@
+"""CrossT-GAN: a reproducible implementation for multivariate anomaly detection."""
+
+from .model import CrossTGAN, MLPDiscriminator
+
+__all__ = ["CrossTGAN", "MLPDiscriminator"]
+
